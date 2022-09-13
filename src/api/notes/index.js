08a84528029c6dbca9d7c9cf3fdb7ef2799e88c9
Dias Utsman <utsmand91@gmail.com>
2022-09-13 08:03:19 +0700

    Add Comments

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -1,9 +1,12 @@
 const NotesHandler = require("./handler")
 const routes = require("./routes")
 
+// The structure of plugin objects
 module.exports = {
     name: 'notes',
     version: '1.0.0',
+    
+    // must have register function with server and server options as its parameter
     async register(server, { service, validator }) {
         const notesHandler = new NotesHandler(service, validator)
         server.route(routes(notesHandler))
